ef375c6190e47a0a8e945f01f1beb87132491667
tran.nguyen16 <laivonguyen@gmail.com>
2023-10-05 01:07:39 +0700

    [fix] add parking entity to map and tunning redisson circuit break

--- src/main/java/org/parkz/modules/parking/mapper/ParkingSlotMapper.java
+++ src/main/java/org/parkz/modules/parking/mapper/ParkingSlotMapper.java
@@ -1,10 +1,16 @@
 package org.parkz.modules.parking.mapper;
 
+import lombok.Setter;
 import org.mapstruct.*;
+import org.parkz.modules.parking.entity.ParkingEntity;
 import org.parkz.modules.parking.entity.ParkingSlotEntity;
 import org.parkz.modules.parking.model.ParkingSlotInfo;
+import org.parkz.modules.parking.repository.ParkingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.fastboot.exception.InvalidException;
 import org.springframework.fastboot.rest.common.mapper.BaseMapper;
 
+@Setter(onMethod_ = {@Autowired})
 @Mapper(
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
@@ -12,6 +18,12 @@ import org.springframework.fastboot.rest.common.mapper.BaseMapper;
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
-public interface ParkingSlotMapper extends BaseMapper<ParkingSlotInfo, ParkingSlotInfo, ParkingSlotEntity> {
+public abstract class ParkingSlotMapper implements BaseMapper<ParkingSlotInfo, ParkingSlotInfo, ParkingSlotEntity> {
+
+    protected ParkingMapper parkingMapper;
+
+    @Override
+    @Mapping(target = "parking", expression = "java(parkingMapper.fromParkingIdToParkingEntity())")
+    public abstract ParkingSlotEntity createConvertToEntity(ParkingSlotInfo detail) throws InvalidException;
 
 }
