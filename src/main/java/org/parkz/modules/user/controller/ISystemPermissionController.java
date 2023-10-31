1112ea9e9868a5ebcb8aaf569d1477263539e298
tran.nguyen16 <laivonguyen@gmail.com>
2023-11-01 00:55:11 +0700

    add filter system permission

--- src/main/java/org/parkz/modules/user/controller/ISystemPermissionController.java
+++ src/main/java/org/parkz/modules/user/controller/ISystemPermissionController.java
@@ -2,11 +2,13 @@ package org.parkz.modules.user.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.parkz.modules.user.model.PermissionInfo;
+import org.parkz.modules.user.model.filter.SystemPermissionFilter;
 import org.springframework.fastboot.rest.common.controller.IGetInfoListController;
+import org.springframework.fastboot.rest.common.controller.IGetInfoListWithFilterController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "System Permission Controller")
 @RequestMapping("/api/v1/system/permissions")
 public interface ISystemPermissionController extends
-        IGetInfoListController<String, PermissionInfo> {
+        IGetInfoListWithFilterController<String, PermissionInfo, SystemPermissionFilter> {
 }
